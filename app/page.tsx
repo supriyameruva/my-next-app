16e5153bba872c6fc9b2b9a57e5ea8ba4e02a8d4
supriya <supriyamp96@gmail.com>
2024-09-20 21:13:10 -0400

    font changed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="h1 p-10 text-xl">Supriya's website</div>
+        <div className="h1 p-10 text-lg">Supriya's website</div>
         </main>
     </div>
   );
